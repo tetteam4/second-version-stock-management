55547a8e19f40c0f9cf3cc806c24e1ccbfa35209
AukTo1380 <auktoafg@gmail.com>
2025-08-23 14:05:14 +0430

    new changedgd

--- api/src/main.jsx
+++ api/src/main.jsx
@@ -7,7 +7,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
 import "./index.css";
 
-// Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
